20cc4bdc72da328ac9b1c1775c3c3753e1d29cda
Noé <neo.laq.33@gmail.com>
2019-04-29 18:12:33 +0200

    Set up screenshake and fov boost

---
+++ MALL_COPS/Assets/Scripts/CameraFunsies/FOVBooster.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVBooster : MonoBehaviour
+{
+    public Camera cam;
+    float lerp = 0.1f;
+    internal float originFOV;
+    float currentFOV;
+    public float resetLerp;
+
+    private void Start()
+    {
+        originFOV = cam.fieldOfView;
+        currentFOV = originFOV;
+    }
+
+    private void FixedUpdate()
+    {
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, currentFOV, lerp);
+    }
+
+    public void SetFOV(float _newFOV, float _lerp)
+    {
+        currentFOV = _newFOV;
+        lerp = _lerp;
+    }
+
+    public void ResetFOV()
+    {
+        currentFOV = originFOV;
+        lerp = resetLerp;
+    }
+}
