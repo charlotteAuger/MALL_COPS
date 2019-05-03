5cd89d3f68f9d0ecd54f03a4ecf5d070daf36a0d
Noé <neo.laq.33@gmail.com>
2019-05-03 11:00:50 +0200

    added visuals and animation controller to AI

---
+++ MALL_COPS/Assets/Scripts/AI/RandomCivilian.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCivilian : MonoBehaviour
+{
+    [SerializeField] private GameObject[] meshes;
+    private GameObject meshObject;
+
+    private void OnEnable()
+    {
+        meshObject = meshes[Random.Range(0, meshes.Length)];
+        meshObject.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        meshObject.SetActive(false);
+    }
+}
